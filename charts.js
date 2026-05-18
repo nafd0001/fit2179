1ad866e092b415a86e030e3ebff10da05ba16f4b
nafd0001 <nafd0001@student.monash.edu>
2026-05-19 00:27:19 +1000

    Update charts.js

--- charts.js
+++ charts.js
@@ -1,15 +1,15 @@
 // Chart 1 -- Rainfall Map
-vegaEmbed('#chart1', 'js/chart1_map.json', { actions: false })
+vegaEmbed('#chart1', 'chart1_map.json', { actions: false })
   .catch(err => console.error('Chart 1 error:', err));
 
 // Chart 2 -- Temperature Map
-vegaEmbed('#chart2', 'js/chart2_map.json', { actions: false })
+vegaEmbed('#chart2', 'chart2_map.json', { actions: false })
   .catch(err => console.error('Chart 2 error:', err));
 
 // Chart 3 -- Temperature Over Time
-vegaEmbed('#chart3', 'js/chart3_line.json', { actions: false })
+vegaEmbed('#chart3', 'chart3_line.json', { actions: false })
   .catch(err => console.error('Chart 3 error:', err));
 
 // Chart 4 -- Rainfall Over Time
-vegaEmbed('#chart4', 'js/chart4_line.json', { actions: false })
+vegaEmbed('#chart4', 'chart4_line.json', { actions: false })
   .catch(err => console.error('Chart 4 error:', err));
