9cb0dc2f66bb5c24f27b5b693df1e4e2d73526b0
nafd0001 <nafd0001@student.monash.edu>
2026-05-31 22:43:55 +1000

    Update main.js

--- main.js
+++ main.js
@@ -678,7 +678,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             'color': {
               'field': 'City', 'type': 'nominal',
               'scale': { 'domain': cityList, 'range': cityList.map(function(c) { return cityColors[c]; }) },
-              'legend': { 'orient': 'bottom', 'title': 'City', 'columns': 6 }
+              'legend': null
             },
             'tooltip': [
               { 'field': 'City',            'type': 'nominal',      'title': 'City' },
